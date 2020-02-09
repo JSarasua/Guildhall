5864552b443fd230decf9f7198e94c99bb7af136
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-02-08 18:12:18 -0600

    Rigidbody now draws an x showing if its enabled or disabled.

--- Engine/Code/Engine/Physics2D/Rigidbody2D.cpp
+++ Engine/Code/Engine/Physics2D/Rigidbody2D.cpp
@@ -1,6 +1,7 @@
 #include "Engine/Physics2D/Rigidbody2D.hpp"
 #include "Engine/Physics2D/Physics2D.hpp"
 #include "Engine/Physics2D/Collider2D.hpp"
+#include "Engine/Renderer/RenderContext.hpp"
 
 
 void Rigidbody2D::Update( float deltaSeconds )
@@ -43,7 +44,10 @@ void Rigidbody2D::TakeCollider( Collider2D* collider )
 void Rigidbody2D::SetPosition( Vec2 const& position )
 {
 	m_worldPosition = position;
-	m_collider->UpdateWorldShape();
+	if( nullptr != m_collider )
+	{
+		m_collider->UpdateWorldShape();
+	}
 }
 
 void Rigidbody2D::SetVelocity( Vec2 const& newVelocity )
@@ -69,6 +73,19 @@ Vec2 Rigidbody2D::GetPosition()
 void Rigidbody2D::DebugRender( RenderContext* context, Rgba8 const& borderColor, Rgba8 const& fillColor )
 {
 	m_collider->DebugRender( context, borderColor, fillColor, 1.f );
+
+	Vec2 positiveOffset = Vec2( 0.5f, 0.5f );
+	Vec2 negativeOffset = Vec2( 0.5f, -0.5f );
+	if( !m_isEnabled )
+	{
+		context->DrawLine(m_worldPosition - positiveOffset, m_worldPosition + positiveOffset, Rgba8::RED, 0.5f);
+		context->DrawLine(m_worldPosition - negativeOffset, m_worldPosition + negativeOffset, Rgba8::RED, 0.5f);
+	}
+	else
+	{
+		context->DrawLine( m_worldPosition - positiveOffset, m_worldPosition + positiveOffset, Rgba8::BLUE, 0.5f );
+		context->DrawLine( m_worldPosition - negativeOffset, m_worldPosition + negativeOffset, Rgba8::BLUE, 0.5f );
+	}
 }
 
 void Rigidbody2D::EnableRigidbody()
