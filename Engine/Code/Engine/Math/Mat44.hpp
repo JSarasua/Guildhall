e4f2aa679ace4bf030cb248efbc1acb8003676a7
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-02-12 16:17:56 -0600

    Implemented CreateOrthographicProjection on Mat44

--- Engine/Code/Engine/Math/Mat44.hpp
+++ Engine/Code/Engine/Math/Mat44.hpp
@@ -89,6 +89,9 @@ public:
 	static const Mat44	CreateUniformScale3D( float uniformScale3D );
 	static const Mat44	CreateNonUniformScale3D( const Vec3& scaleFactorsXYZ);
 
+	// projection
+	static const Mat44 CreateOrthographicProjection( const Vec3& min, const Vec3& max );
+
 private:
 	const Mat44 operator*( const Mat44& rhs ) const = delete; //FORBIDDEN
 };
