a8b2c30004a893ebb75cca1b3c49a1d95173091f
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-03-09 14:09:18 -0500

    Added in rotation variables to Rigidbody

--- Engine/Code/Engine/Physics2D/Rigidbody2D.hpp
+++ Engine/Code/Engine/Physics2D/Rigidbody2D.hpp
@@ -69,4 +69,10 @@ protected:
 
 	Vec2 m_forcePerFrame = Vec2(0.f,0.f);
 	eSimulationMode m_simulationMode = DYNAMIC;
+
+	float m_rotationInRadians = 0.f;	//Radians
+	float m_angularVelocity = 0.f;		//Radians/Sec
+	float m_frameTorque = 0.f;			//???
+	float m_moment = 0.f;				//Moment of Inertia
+
 };
