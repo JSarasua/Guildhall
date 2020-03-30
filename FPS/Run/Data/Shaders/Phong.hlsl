19b7539ac4ef13e14b0584ec3fe0c5c8fcb7e5e9
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-03-30 15:58:12 -0500

    Created beginning of Phong shader, doesn't work right now.

---
+++ FPS/Run/Data/Shaders/Phong.hlsl
@@ -0,0 +1,146 @@
+//--------------------------------------------------------------------------------------
+// Stream Input
+// ------
+// Stream Input is input that is walked by the vertex shader.  
+// If you say "Draw(3,0)", you are telling to the GPU to expect '3' sets, or 
+// elements, of input data.  IE, 3 vertices.  Each call of the VertxShader
+// we be processing a different element. 
+//--------------------------------------------------------------------------------------
+
+// inputs are made up of internal names (ie: uv) and semantic names
+// (ie: TEXCOORD).  "uv" would be used in the shader file, where
+// "TEXCOORD" is used from the client-side (cpp code) to attach ot. 
+// The semantic and internal names can be whatever you want, 
+// but know that semantics starting with SV_* usually denote special 
+// inputs/outputs, so probably best to avoid that naming.
+struct vs_input_t 
+{
+	// we are not defining our own input data; 
+	float3 position      : POSITION; 
+	float4 color         : COLOR; 
+	float2 uv            : TEXCOORD; 
+}; 
+
+static float SHIFT = 0.75f;
+cbuffer time_constants : register(b0)	//Index 0 is time
+{
+	float SYSTEM_TIME_SECONDS;
+	float SYSTEM_TIME_DELTA_SECONDS;
+};
+
+cbuffer camera_constants : register(b1)
+{
+//	float2 orthoMin;
+//	float2 orthoMax;
+	float4x4 PROJECTION; // CAMERA_TO_CLIP_TRANSFORM in the model view projection
+	float4x4 VIEW;
+};
+
+cbuffer model : register(b2)
+{
+	float4x4 MODEL;
+	float4 TINT;
+};
+
+//NEW
+struct light_t
+{
+	float3 worldPosition;
+	float pad00; //not required
+
+	float3 color
+	float intensity;
+};
+
+cbuffer lightConstants : register(b3)
+{
+	float4 AMBIENT;
+	light_t LIGHT;
+}
+
+
+// Textures & Samplers are also a form of constant
+// data - unform/constant across the entire call
+// tSlots are for big things
+Texture2D <float4> tDiffuse	: register(t0);	// color of the surface
+SamplerState sSampler : register(s0);		// sampler are rules on how to sample color per pixel
+
+//--------------------------------------------------------------------------------------
+// Programmable Shader Stages
+//--------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------
+// for passing data from vertex to fragment (v-2-f)
+struct v2f_t 
+{
+	float4 position : SV_POSITION; 
+	float4 color : COLOR; 
+	float2 uv : UV; 
+	float3 worldPosition : WORLDPOS;
+}; 
+
+float RangeMap( float val, float inMin, float inMax, float outMin, float outMax )
+{
+	float inRange = inMax - inMin;
+	float outRange = outMax - outMin;
+	return ((val - inMin) / inRange) * outRange + outMin;
+}
+
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+v2f_t VertexFunction( vs_input_t input )
+{
+	v2f_t v2f = (v2f_t)0;
+
+	// forward vertex input onto the next stage
+	v2f.position = float4( input.position, 1.0f ); 
+	v2f.color = input.color * TINT; 
+	v2f.uv = input.uv; 
+
+	float4 worldPos = float4( input.position, 1);
+
+	v2f.worldPosition = worldPos.xyz;
+
+
+	//Need to add a normal in with same operations vs_input_t should be replaced to hold a normal and named Vertex_PCUTBN
+	float4 modelPos = mul(MODEL, worldPos);
+	float4 cameraPos = mul(VIEW, modelPos);
+	float4 clipPos = mul(PROJECTION, cameraPos);	// might have a w (usually 1 for now)
+
+	v2f.position = clipPos;
+
+	return v2f;
+}
+
+//--------------------------------------------------------------------------------------
+// Fragment Shader
+// 
+// SV_Target0 at the end means the float4 being returned
+// is being drawn to the first bound color target.
+float4 FragmentFunction( v2f_t input ) : SV_Target0
+{
+	float4 color = tDiffuse.Sample( sSampler, input.uv );
+	return color * input.color;
+
+
+	//new way
+	/*
+	float4 textureColor = tDiffuse.Sample( sSampler, input.uv );
+	float3 surfaceColor = (input.Color * textureColor).xyz;
+	float3 surfaceAlpha = (input.Color.a)
+	
+		
+	float3 diffuse = AMBIENT.xyz * AMBIENT.w;
+	float3 surfaceNormal = ???;
+	float3 lightPosition = LIGHT.worldPosition;
+	float3 dirToLight = normalize( lightPosition - input.worldPosition );
+
+	float dot3 = max( 0.f, dot( dirToLight, surfaceNormal ) );
+	diffuse += dot3;
+
+	diffuse = saturate(diffuse);
+
+	float3 finalColor = (input.color.a * textureColor.a );
+	return float4( finalColor, surfaceAlpha );
+	*/
+}
