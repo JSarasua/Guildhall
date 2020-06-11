612756f600a783a45a55252263570c82554d7bb7
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-06-10 23:36:10 -0500

    MapTiles now construct with a MapRegionType and have IsSolid

--- Doomenstein/Code/Game/MapTile.cpp
+++ Doomenstein/Code/Game/MapTile.cpp
@@ -1,7 +1,13 @@
 #include "Game/MapTile.hpp"
+#include "Game/MapRegionType.hpp"
 
-MapTile::MapTile( const IntVec2& tileCoords ) : 
-	m_tileCoords( tileCoords )
+MapTile::MapTile( const IntVec2& tileCoords, MapRegionType* mapRegionType ) : 
+	m_tileCoords( tileCoords ), m_mapRegionType( mapRegionType )
 {
 
+}
+
+bool MapTile::IsSolid() const
+{
+	return m_mapRegionType->IsSolid();
 }
--- Doomenstein/Code/Game/MapTile.hpp
+++ Doomenstein/Code/Game/MapTile.hpp
@@ -5,13 +5,17 @@
 struct AABB2;
 struct Vertex_PCU;
 
+class MapRegionType;
+
 class MapTile
 {
 public:
 	MapTile() {}
 	~MapTile() {}
 
-	MapTile( const IntVec2& tileCoords );
+	MapTile( const IntVec2& tileCoords, MapRegionType* mapRegionType );
+
+	bool IsSolid() const;
 
 
 /*	AABB2 GetAABB2() const;*/
@@ -19,4 +23,5 @@ public:
 public:
 	IntVec2 m_tileCoords;
 	bool m_tempIsSolid = false;
+	MapRegionType* m_mapRegionType = nullptr;
 };
