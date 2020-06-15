0b9acefab8d9ea2bb6107408df14e068e885a2d8
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-06-15 18:22:01 -0500

    Create all maps of game

--- Doomenstein/Code/Game/World.cpp
+++ Doomenstein/Code/Game/World.cpp
@@ -1,7 +1,7 @@
 #include "Game/World.hpp"
 #include "Game/Game.hpp"
 #include "Game/TileMap.hpp"
-
+#include "Engine/Core/StringUtils.hpp"
 
 
 World::World( Game* game ) :
@@ -12,15 +12,28 @@ World::World( Game* game ) :
 
 void World::Startup()
 {
-	XmlDocument singleBlockMap		= new XmlDocument;
-	XmlElement const& singleBlockMapDef = GetRootElement( singleBlockMap, "Data/Maps/TwistyMaze.xml");
-	//Create 
-	m_currentMap = new TileMap( singleBlockMapDef, m_game );
+	Strings filePaths = GetFileNamesInFolder( "Data/Maps", "*.xml" );
+
+	for( size_t fileIndex = 0; fileIndex < filePaths.size(); fileIndex++ )
+	{
+		std::string filePath = "Data/Maps/" + filePaths[fileIndex];
+		XmlDocument currentMapDoc		= new XmlDocument;
+		XmlElement const& currentMapDefElement = GetRootElement( currentMapDoc, filePath.c_str() );
+		Map* newMap = new TileMap( currentMapDefElement, m_game );
+		m_maps.push_back( newMap );
+	}
+	
+	m_currentMap = m_maps[0];
 }
 
 void World::Shutdown()
 {
-	delete m_currentMap;
+	for( size_t mapIndex = 0; mapIndex < m_maps.size(); mapIndex++ )
+	{
+		delete m_maps[mapIndex];
+		m_maps[mapIndex] = nullptr;
+	}
+	//delete m_currentMap;
 }
 
 void World::Update( float deltaSeconds )
@@ -31,5 +44,11 @@ void World::Update( float deltaSeconds )
 void World::Render()
 {
 	m_currentMap->Render();
+}
+
+void World::SetCurrentMap( Map* newCurrentMap )
+{
+	m_currentMap = newCurrentMap;
+	//Update cameraPosition;
 }
 
--- Doomenstein/Code/Game/World.hpp
+++ Doomenstein/Code/Game/World.hpp
@@ -23,7 +23,11 @@ public:
 	void Update( float deltaSeconds );
 	void Render();
 
+	void SetCurrentMap( Map* newCurrentMap );
+
 private:
+	std::vector<Map*> m_maps;
+
 	Map* m_currentMap = nullptr;
 	Game* m_game = nullptr;
 public:
