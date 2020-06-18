21df316ae9f06be46e1908644c7483a4f39e7a1c
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-06-18 00:00:14 -0500

    Created EntityDefinition

---
+++ Doomenstein/Code/Game/EntityDefinition.cpp
@@ -0,0 +1,14 @@
+#include "Game/EntityDefinition.hpp"
+
+std::map< std::string, EntityDefinition* > EntityDefinition::s_definitions;
+
+EntityDefinition::EntityDefinition( XmlElement const& element )
+{
+
+}
+
+void EntityDefinition::InitializeEntityDefinitions( XmlElement const& rootEntityDefinitionElement )
+{
+
+}
+
---
+++ Doomenstein/Code/Game/EntityDefinition.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "Engine/Core/XmlUtils.hpp"
+#include <map>
+#include <string>
+
+
+
+class EntityDefinition
+{
+public:
+	EntityDefinition() = delete;
+	EntityDefinition( XmlElement const& element );
+	~EntityDefinition() {}
+
+	static void InitializeEntityDefinitions( XmlElement const& rootEntityDefinitionElement );
+	static std::map< std::string, EntityDefinition* > s_definitions;
+
+
+
+protected:
+	std::string m_name;
+	std::string m_type;
+	bool m_isValid = false;
+
+	bool m_canBePushedByWalls = false;
+	bool m_canBePushedByEntities = false;
+	bool m_canPushEntities = false;
+	float m_mass = 1.f;
+
+};
