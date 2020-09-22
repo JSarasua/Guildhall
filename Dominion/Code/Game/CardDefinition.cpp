6473fec86e56fadb20f1581c46aa64489016deb7
Sarasua, Jonathan <jsarasua@mail.smu.edu>
2020-09-21 23:32:45 -0500

    Initialized card definitions

--- Dominion/Code/Game/CardDefinition.cpp
+++ Dominion/Code/Game/CardDefinition.cpp
@@ -2,11 +2,12 @@
 #include "Engine/Core/EngineCommon.hpp"
 //#include "Engine/Core/StringUtils.hpp"
 
-std::map< int, CardDefinition* > CardDefinition::s_definitions;
+std::map< eCards, CardDefinition* > CardDefinition::s_definitions;
 
-CardDefinition::CardDefinition( int cardIndex, int cost, int type, std::string cardName, int vp, int plusActions, int plusDraw ) :
+CardDefinition::CardDefinition( int cardIndex, int type, std::string cardName, int cost, int coins, int vp, int plusActions, int plusDraw ) :
 	m_cardIndex( cardIndex ), 
 	m_cost( cost ),
+	m_coins( coins ),
 	m_type( type ),
 	m_cardName( cardName ),
 	m_actionsGained( plusActions ),
@@ -14,17 +15,36 @@ CardDefinition::CardDefinition( int cardIndex, int cost, int type, std::string c
 	m_victoryPointValue( vp )
 {}
 
+void CardDefinition::InitializeCards()
+{																													//Cost		Coins	VP		Actions	Draw
+	CardDefinition* copperCard		= new CardDefinition( eCards::COPPER,	eCardType::TREASURE_TYPE,	"Copper",		 0,			1,		 0,		 0,		0 );
+	CardDefinition* silverCard		= new CardDefinition( eCards::SILVER,	eCardType::TREASURE_TYPE,	"Silver",		 3,			2,		 0,		 0,		0 );
+	CardDefinition* goldCard		= new CardDefinition( eCards::GOLD,		eCardType::TREASURE_TYPE,	"Gold",			 6,			3,		 0,		 0,		0 );
+	CardDefinition* estateCard		= new CardDefinition( eCards::ESTATE,	eCardType::VICTORY_TYPE,	"Estate",		 2,			0,		 1,		 0,		0 );
+	CardDefinition* duchyCard		= new CardDefinition( eCards::DUCHY,	eCardType::VICTORY_TYPE,	"Duchy",		 5,			0,		 3,		 0,		0 );
+	CardDefinition* provinceCard	= new CardDefinition( eCards::PROVINCE, eCardType::VICTORY_TYPE,	"Province",		 8,			0,		 6,		 0,		0 );
+	CardDefinition* curseCard		= new CardDefinition( eCards::CURSE,	eCardType::CURSE_TYPE,		"Curse",		 0,			0,		 -1,	 0,		0 );
+
+	s_definitions[eCards::COPPER]	= copperCard;
+	s_definitions[eCards::SILVER]	= silverCard;
+	s_definitions[eCards::GOLD]		= goldCard;
+	s_definitions[eCards::ESTATE]	= estateCard;
+	s_definitions[eCards::DUCHY]	= duchyCard;
+	s_definitions[eCards::PROVINCE] = provinceCard;
+	s_definitions[eCards::CURSE]	= curseCard;
+}
+
 //STATIC
-CardDefinition const* CardDefinition::GetCardDefinitionByIndex( int cardIndex )
+CardDefinition const* CardDefinition::GetCardDefinitionByType( eCards cardType )
 {
-	auto cardIter = s_definitions.find( cardIndex );
+	auto cardIter = s_definitions.find( cardType );
 	if( cardIter != s_definitions.end() )
 	{
 		return cardIter->second;
 	}
 	else
 	{
-		g_theConsole->ErrorString( Stringf( "Eror: Could not find card type with index: %i", cardIndex ) );
+		g_theConsole->ErrorString( Stringf( "Eror: Could not find card type with index: %i", (int)cardType ) );
 		return nullptr;
 	}
 }
--- Dominion/Code/Game/CardDefinition.hpp
+++ Dominion/Code/Game/CardDefinition.hpp
@@ -5,9 +5,10 @@
 enum eCardType
 {
 	InvalidType = -1,
-	Action,
-	Treasure,
-	Victory,
+	ACTION_TYPE,
+	CURSE_TYPE,
+	TREASURE_TYPE,
+	VICTORY_TYPE,
 
 	NumCardTypes
 };
@@ -26,6 +27,8 @@ enum eCards
 	DUCHY,
 	PROVINCE,
 
+	//Curse
+	CURSE,
 	//Action
 
 	NUM_CARDS
@@ -36,20 +39,23 @@ class CardDefinition
 {
 public:
 	CardDefinition() = delete;
-	CardDefinition( int cardIndex, int cost, int type, std::string cardName, int vp = 0, int plusActions = 0, int plusDraw = 0 );
+	CardDefinition( int cardIndex, int type, std::string cardName, int cost = 0, int coins = 0, int vp = 0, int plusActions = 0, int plusDraw = 0 );
 	~CardDefinition() {}
 
-	static CardDefinition const* GetCardDefinitionByIndex( int cardIndex );
-	static std::map< int, CardDefinition* > s_definitions;
+	static void InitializeCards();
+	static CardDefinition const* GetCardDefinitionByType( eCards cardType );
+	static std::map< eCards, CardDefinition* > s_definitions;
 
 	int GetCardCost() const { return m_cost; }
 	int GetCardType() const { return m_type; }
+	int GetCardVPs() const	{ return m_victoryPointValue; }
 	std::string const& GetCardName() const { return m_cardName; }
 
 public:
 	int m_cardIndex = -1;
 	int m_cost = -1;
 	int m_type = -1;
+	int m_coins = -1;
 	std::string m_cardName;
 	int m_victoryPointValue = 0;
 	int m_actionsGained = 0;
